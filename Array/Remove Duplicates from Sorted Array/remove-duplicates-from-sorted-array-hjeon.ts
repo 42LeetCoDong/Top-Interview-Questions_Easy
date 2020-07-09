a5c688ce923bde2028caaf4238c4b7298c467f7e
hwano <jho2301@gmail.com>
2020-07-09 22:19:30 +0900

    Update remove-duplicates-from-sorted-array-hjeon.ts

--- Array/Remove Duplicates from Sorted Array/remove-duplicates-from-sorted-array-hjeon.ts
+++ Array/Remove Duplicates from Sorted Array/remove-duplicates-from-sorted-array-hjeon.ts
@@ -1,7 +1,6 @@
 function removeDuplicates(nums: number[]): number {
-	for (let i = 0; i < nums.length; i++){
-		if (0 < i && nums[i - 1] === nums[i])
+	for (let i = 1; i < nums.length; i++)
+		if (nums[i - 1] === nums[i])
 			nums.splice(i--, 1);
-	}
 	return (nums.length);
 };
