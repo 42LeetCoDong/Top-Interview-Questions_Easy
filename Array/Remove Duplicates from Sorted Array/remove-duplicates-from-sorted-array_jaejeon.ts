62196c2d04964c0589f3f469af8eabd74bf8d90e
Jayten <jaejeon@student.42seoul.kr>
2020-07-09 20:17:27 +0900

    Update remove-duplicates-from-sorted-array_jaejeon.ts

--- Array/Remove Duplicates from Sorted Array/remove-duplicates-from-sorted-array_jaejeon.ts
+++ Array/Remove Duplicates from Sorted Array/remove-duplicates-from-sorted-array_jaejeon.ts
@@ -1,3 +1,20 @@
 function removeDuplicates(nums: number[]): number {
+    let curElm: number;
+    let nextIdx: number;
+    let n: number;
 
+    n = nums.length;
+    curElm = nums[0];
+    nextIdx = 1;
+    for  (let num of nums.slice(1))  {
+        if (curElm === num){
+            continue;
+        }
+        curElm = num;
+        nums[nextIdx] = curElm;
+        nextIdx++;
+    }
+    nums = nums.slice(0, nextIdx);
+    
+    return nums.length;
 };
