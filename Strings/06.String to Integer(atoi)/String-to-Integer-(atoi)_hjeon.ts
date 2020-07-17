bdaa38b4883e7528d0ec25d38e69a9fbac8186a8
hwano <jho2301@gmail.com>
2020-07-17 09:49:01 +0900

    Update String-to-Integer-(atoi)_hjeon.ts

--- Strings/06.String to Integer(atoi)/String-to-Integer-(atoi)_hjeon.ts
+++ Strings/06.String to Integer(atoi)/String-to-Integer-(atoi)_hjeon.ts
@@ -8,18 +8,12 @@ function myAtoi(str: string): number {
 		if (str[0] === '-') isMinus = true;
 		str = str.slice(1);
 	}
-	console.log(str)
 	for (let i = 0; str[i] && str[i].match(/[0-9]/); i++) {
 		result *= 10;
 		result += parseInt(str[i]);
 	}
 	result = (isMinus) ? -result : result;
-	console.log('result:', result)
 	if (result < -(2 ** 31)) result = -(2 ** 31);
-	if ((2 ** 31) - 1 < result) result = (2 * 31) - 1;
+	if ((2 ** 31) - 1 < result) result = (2 ** 31) - 1;
 	return (result);
 };
-
-let input: string = "4193 with words"
-console.log("myAtoi    : ", myAtoi(input))
-console.log("parse INT : ", parseInt(input));
