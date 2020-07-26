db184ff823cd93749ce01e0af409d23953b60930
hwano <jho2301@gmail.com>
2020-07-26 20:01:53 +0900

    Add files via upload

---
+++ Dynamic Programming/01. Climbing Stairs/Climbing-Stairs_hjeon.ts
@@ -0,0 +1,11 @@
+function climbStairs(n: number): number {
+  let dp: number[] = new Array(n + 1) ;
+
+  dp[1] = 1;
+  dp[2] = 2;
+  for (let i = 3; i <= n; i++)
+      dp[i] = dp[i - 1] + dp[i - 2];
+  return dp[n];
+};
+
+console.log(climbStairs(23))
